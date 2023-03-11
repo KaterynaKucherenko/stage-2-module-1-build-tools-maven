5370c99e7d3f86f91d7510a163c0d146bef21a46
KaterynaKucherenko <estorskaya@gmail.com>
2023-03-11 19:34:41 +0300

    done

--- demo/src/main/java/com/epam/demo/Utils.java
+++ demo/src/main/java/com/epam/demo/Utils.java
@@ -1,9 +1,23 @@
 package com.epam.demo;
 
+import org.apache.commons.lang3.math.NumberUtils;
+
 import java.util.List;
 
 public class Utils {
     public static boolean isAllPositiveNumbers(List<String> args) {
        //magic happens here
-    }
-}
+        try{  for(String i: args){
+            if  (Float.parseFloat(i)<=0){
+                return false;}
+            if  (!NumberUtils.isNumber(i)){
+                return false;}
+
+        }
+        }
+        catch (Exception e){
+            return false;
+        }
+        return true;
+    }}
+
--- utils/src/main/java/com/epam/utils/StringUtils.java
+++ utils/src/main/java/com/epam/utils/StringUtils.java
@@ -1,7 +1,16 @@
 package com.epam.utils;
-
+import org.apache.commons.lang3.math.NumberUtils;
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        //code!
+       try{     if(!NumberUtils.isNumber(str)){
+                return false;}
+            if(Float.parseFloat(str)<=0){
+                return false;}
+
+        }
+       catch(Exception e){
+           return false;}
+       return true;
+       }
     }
-}
+
